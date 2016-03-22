2e78ec700759d3e25566cbe8d76e1430a3f62e30
borna.koruznjak <borna.koruznjak@gmail.com>
2016-03-22 22:52:59 +0100

    Initial commit

---
+++ app/src/main/java/hr/from/bkoruznjak/top10downloader/model/Application.java
@@ -0,0 +1,40 @@
+package hr.from.bkoruznjak.top10downloader.model;
+
+/**
+ * Created by Borna on 12.3.16.
+ */
+public class Application {
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" + "Artist: " + getArtist() + "\n" + "Release date: " + getReleaseDate();
+
+    }
+}
